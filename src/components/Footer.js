5f22cb0e41efcecd966824be8516c461f5a203f9
Bhargav Panchal <bhargav.panchal222@gmail.com>
2024-10-24 17:36:32 -0400

    links working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                 </div>
                 
                 <div className="phone">
-                    <h3><FaPhone size={25} style={{color:"white", marginRight: "1.5rem"}}/>xxx-xxx-xxxx</h3>
+                    <h3><FaPhone size={25} style={{color:"white", marginRight: "1.5rem"}}/>(804) 665 8361</h3>
                 </div>
                 
                 <div className="email">
@@ -25,8 +25,12 @@ const Footer = () => {
                 <h4>About Me</h4>
                 <p> I am Bhargav Panchal, a software developer with a Bachelor’s degree in Computer Science from Virginia Tech and an Associate’s degree from Reynolds Community College. I specialize in Java, Python, JavaScript, HTML, CSS, and the MERN stack, focusing on creating innovative solutions to enhance user experiences. </p>
                 <div className="social">
-                    <FaLinkedin size={20} style={{color:"white", marginRight: "2rem"}}/>
-                    <FaGithub size={20} style={{color:"white", marginRight: "2rem"}}/>
+                    <a href="https://www.linkedin.com/in/bhargav-panchal-219bb0185" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} style={{color: "white", marginRight: "2rem"}}/>
+                    </a>
+                    <a href="https://github.com/bhargav149" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={20} style={{color: "white", marginRight: "2rem"}}/>
+                    </a>
                 </div>
                     
             </div>
